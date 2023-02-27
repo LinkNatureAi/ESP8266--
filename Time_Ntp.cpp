0671ac1fd3643d4cb693f7359a5b643724a4e352
SONU KOL <108781856+SONUKOL1@users.noreply.github.com>
2023-02-27 14:37:03 +0530

    Create Time_Ntp.cpp

---
+++ Time_Ntp.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+#define ssid     "OPPO"
+#define password  "123456789"
+#define ntpServer  "pool.ntp.org"
+#define gmtOffset_sec  19800 // Indian Standard Time (IST) is GMT+5:30
+#define daylightOffset_sec  0
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500); Serial.print(".");  }
+Serial.println(" Connected to ");         
+Serial.print(" hotspot: ");     
+Serial.println(WiFi.localIP());              
+Serial.print(" wifi: ");    
+Serial.println(WiFi.softAPIP());           
+Serial.print(" Signal Strength ");           
+Serial.print(WiFi.RSSI());              
+Serial.println(" dBm");              
+Serial.print("Router IP ");          
+Serial.println(WiFi.gatewayIP());                 
+Serial.print(" Device MAC ");              
+Serial.println(WiFi.macAddress());
+  // Initialize time with NTP server
+configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  // Wait for time to be set
+  time_t now = time(nullptr);
+  while (now < 1000) {
+    delay(1000);
+    Serial.println("Waiting for time synchronization...");
+    now = time(nullptr);
+  }
+}
+
+void loop() {
+  delay(1000);
+  time_t now = time(nullptr);
+  struct tm timeinfo;
+  localtime_r(&now, &timeinfo);
+char time_str[64];
+  strftime(time_str, sizeof(time_str), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+  Serial.println(time_str);
+}  
