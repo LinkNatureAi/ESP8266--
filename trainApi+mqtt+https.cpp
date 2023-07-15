13755dcbd29d1cc2694c362f4ca5a1f01ef51384
SONU KOL <108781856+LinkNatureAi@users.noreply.github.com>
2023-07-15 13:47:13 +0530

    Update trainApi+mqtt+https.cpp

--- trainApi+mqtt+https.cpp
+++ trainApi+mqtt+https.cpp
@@ -1,4 +1,3 @@
-//this code build LinkNatureAi(SONU KOL)
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecureBearSSL.h>
@@ -8,11 +7,11 @@ const char* ssid = "POCO X3 Pro";
 const char* password = "1234567890";
 const char* mqttServer = "broker.hivemq.com";
 const char* clientID = "EspClient";
-const char* SendTrain = "train";
-const char* train_num = "train-num";
-
-String train_var = "18233/2";
-String http_resp;
+const char* trainTopic = "train2";
+const char* trainNumTopic = "train-num2";
+const String defaultTrainVar = "18234/2";
+String trainVar;
+String httpResp;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -20,28 +19,36 @@ void reconnect() {
   while (!client.connected()) {
     if (client.connect((clientID + String(millis())).c_str())) {
       Serial.println("MQTT connected");
-      client.subscribe(train_num);
+      client.subscribe(trainNumTopic);
     } else {
-      Serial.printf("failed, rc=%d, try again in 5 seconds\n", client.state());
-      delay(5000);
+      Serial.printf("MQTT connection failed, rc=%d. Retrying in 5 seconds...\n", client.state());
+      delay(3000);
     }
   }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  String rec = String((char*)payload).substring(0, length);
-  Serial.printf("topic=[%s] message=[%s]\n", topic, rec.c_str());
+  String rec;
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    rec += (char)payload[i];
+  }
+  Serial.print(" topic=[");
+  Serial.print(topic);
+  Serial.print("]  message=[");
+  Serial.print(rec);
+  Serial.println("]");
 
-  if (strstr(topic, train_num)) {
-    train_var = String(rec);
+  if (strcmp(topic, trainNumTopic) == 0) {
+    trainVar = rec;
   } else {
-    Serial.println("unsubscribed topic");
+    Serial.println("Unsubscribed topic");
   }
 }
 
-void man_control() {
-  client.publish(SendTrain, String(http_resp).c_str());
-  delay(3000);
+void manualControl() {
+  client.publish(trainTopic, httpResp.c_str());
+  delay(2000);
 }
 
 void setup() {
@@ -49,52 +56,58 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("WiFi connecting.");
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000); Serial.print(".");
+    delay(1000);
+    Serial.print(".");
   }
 
   Serial.println("\nWiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
   client.setServer(mqttServer, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
+  if (trainVar < "0") {
+    trainVar = defaultTrainVar;
+  }
+
   if (!client.connected()) {
     reconnect();
   } else {
-    man_control();
+    manualControl();
   }
   client.loop();
 
-   if (WiFi.status() == WL_CONNECTED) {
-    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-    client->setInsecure();
-    
+  if (WiFi.status() == WL_CONNECTED) {
+    BearSSL::WiFiClientSecure client;
+    client.setInsecure();
+
     HTTPClient https;
-    Serial.print("[HTTPS] begin...\n");
-    
-    if (https.begin(*client, "https://train-54ut.onrender.com/"+String(train_var))) {
+    Serial.print("[HTTPS] Connecting...\n");
+                            // https://train-54ut.onrender.com/ 
+    if (https.begin(client, "https://repmulti.sonukol.repl.co/" + trainVar)) {
       Serial.print("[HTTPS] GET...\n");
       int httpCode = https.GET();
-      
+
       if (httpCode > 0) {
         Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-        
+
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          http_resp = https.getString();
-          Serial.println(http_resp);
+          httpResp = https.getString();
+          Serial.println(httpResp);
         }
       } else {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
-      
+
       https.end();
     } else {
       Serial.printf("[HTTPS] Unable to connect\n");
     }
   }
- Serial.println("\n");
+
+  Serial.println();
 }
